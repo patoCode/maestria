4bcfdf30686388dfae85b3c7081929be9ae7918d
patoCode <e3gan.root@gmail.com>
2018-09-23 23:09:48 -0400

    Mejora Clena Code - Aplicacion de SOLID

--- operacion.php
+++ operacion.php
@@ -1,29 +1,67 @@
 <?php
+// Se creo una interfaz llamada Operacion, el cual agrupara por comportamiento las operaciones
 
-class operacion
-{
-    private static $instancia;
-    private $contar;
-
-    private function __construct(){      
-      $this->contar = 0;
-    } 
-    public static function invocarinstancia(){
-        if (  !self::$instancia instanceof self){ 
-         self::$instancia = new self;
-        } 
-      return self::$instancia;
+interface OperacionSuma{
+    public function doSuma($numero);
+}
+interface OperacionResta{
+    public function doResta($numero);
+}
+abstract class Operacion
+{ 
+    public $numero;
+    public function __construct($numero)
+    {
+        $this->numero = $numero;
     }
-    public function sumar(){
-        return ++$this->contar;
-    }
-    public function restar(){
-       return --$this->contar;
-   } 
 }
 
-$calculo = operacion::invocarinstancia();
-echo "Invocar sumar: ".$calculo->sumar() . "\n";
-echo "Invocar suma: ".$calculo->sumar() . "\n";
-echo "Invocar resta: ".$calculo->restar() . "\n";
-echo "Invocar resta: ".$calculo->restar() . "\n";
+class Suma extends Operacion implements OperacionSuma{    
+    public function __construct($numero){
+        parent::__construct($numero);
+    }
+    public function doSuma($sumando){
+        echo "SUMA ".$this->numero." + ".$sumando." = ".($this->numero+$sumando);
+    }    
+}
+class Resta extends Operacion implements OperacionResta{
+    public function __construct($numero){
+        parent::__construct($numero);
+    }
+    public function doResta($sustraendo){
+        echo "RESTA ".$this->numero." - ".$sustraendo." = ".($this->numero-$sustraendo);
+    }
+}
+
+function operar($operacion, $numero_para_operar) {
+    if ($operacion instanceof Operacion) {
+      if ($operacion instanceof OperacionSuma) {
+        $operacion->doSuma($numero_para_operar);
+      }
+      if ($operacion instanceof OperacionResta) {
+        $operacion->doResta($numero_para_operar);
+      }       
+    } else {
+      die("No me enviaste un operador");
+    }
+}
+
+
+echo "===========================\n";
+echo "OPERACIONES SUMA Y RESTA\n";
+echo "===========================\n";
+$razon_de_cambio = 10;
+$operaciones = array(
+    new Suma(20),
+    new Suma(15),
+    new Resta(150),
+    new Suma(1),
+    new Suma(30),
+    new Resta(5),
+    new Resta(15)
+  );
+foreach($operaciones as $operacion) 
+{
+    operar($operacion,$razon_de_cambio);
+    echo "\n";
+}
